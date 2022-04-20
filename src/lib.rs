eb48de02a5186767d9527a110f4375ec6555422f
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2022-04-20 05:44:40 +0300

    Adapt for OsStr/OsString
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@
 // Licensed under the Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0> or
 // the MIT license <https://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
+//
 
+//! Based on the [shlex](https://crates.io/crates/shlex), adapted for OsStr/OsString.
+//!
+//! -------
+//!
 //! Same idea as (but implementation not directly based on) the Python shlex module.  However, this
 //! implementation does not support any of the Python module's customization because it makes
 //! parsing slower and is fairly useless.  You only get the default settings of shlex.split, which
@@ -18,21 +23,45 @@
 //! Disabling the `std` feature (which is enabled by default) will allow the crate to work in
 //! `no_std` environments, where the `alloc` crate, and a global allocator, are available.
 
-#![cfg_attr(not(feature = "std"), no_std)]
 
-extern crate alloc;
-use alloc::vec::Vec;
-use alloc::borrow::Cow;
-use alloc::string::String;
-#[cfg(test)]
-use alloc::vec;
-#[cfg(test)]
-use alloc::borrow::ToOwned;
+use std::borrow::Cow;
+use std::ffi::{OsStr, OsString};
+use std::vec;
+
+use os_str_bytes::{OsStrBytes, OsStringBytes};
+use os_str_bytes::RawOsString;
+
+// TODO: Use `Iterator::join` instead when implemented:
+//       https://github.com/rust-lang/rust/issues/75638
+struct MultiOsStrJoinerWrapper<'a> {
+    inner: Vec<Cow<'a, OsStr>>,
+}
+
+impl<'a> MultiOsStrJoinerWrapper<'a> {
+    fn into_joined(self, sep: &OsStr) -> OsString {
+        if self.inner.is_empty() {
+            return OsString::default();
+        }
+
+        let capacity = self.inner
+            .iter()
+            .map(|s| s.len() + sep.len())
+            .sum::<usize>() - sep.len();
+        let mut ret = OsString::with_capacity(capacity);
+        let mut iter = self.inner.into_iter();
+        ret.push(iter.next().expect("impossible due to is_empty() check above"));
+        iter.for_each(|s| {
+            ret.push(sep);
+            ret.push(s);
+        });
+        ret
+    }
+}
 
 /// An iterator that takes an input string and splits it into the words using the same syntax as
 /// the POSIX shell.
-pub struct Shlex<'a> {
-    in_iter: core::str::Bytes<'a>,
+pub struct ShlexOs {
+    in_iter: vec::IntoIter<u8>,
     /// The number of newlines read so far, plus one.
     pub line_no: usize,
     /// An input string is erroneous if it ends while inside a quotation or right after an
@@ -42,60 +71,62 @@ pub struct Shlex<'a> {
     pub had_error: bool,
 }
 
-impl<'a> Shlex<'a> {
-    pub fn new(in_str: &'a str) -> Self {
-        Shlex {
-            in_iter: in_str.bytes(),
+impl ShlexOs {
+    pub fn new(in_string: impl Into<OsString>) -> Self {
+        let raw = RawOsString::new(in_string.into());
+        Self {
+            in_iter: raw.into_raw_vec().into_iter(),
             line_no: 1,
             had_error: false,
         }
+        //Self::_new(raw)
     }
 
-    fn parse_word(&mut self, mut ch: u8) -> Option<String> {
+    fn parse_word(&mut self, mut ch: u8) -> Option<OsString> {
         let mut result: Vec<u8> = Vec::new();
         loop {
-            match ch as char {
-                '"' => if let Err(()) = self.parse_double(&mut result) {
+            match ch {
+                b'"' => if let Err(()) = self.parse_double(&mut result) {
                     self.had_error = true;
                     return None;
                 },
-                '\'' => if let Err(()) = self.parse_single(&mut result) {
+                b'\'' => if let Err(()) = self.parse_single(&mut result) {
                     self.had_error = true;
                     return None;
                 },
-                '\\' => if let Some(ch2) = self.next_char() {
-                    if ch2 != '\n' as u8 { result.push(ch2); }
+                b'\\' => if let Some(ch2) = self.next_char() {
+                    if ch2 != b'\n' { result.push(ch2); }
                 } else {
                     self.had_error = true;
                     return None;
                 },
-                ' ' | '\t' | '\n' => { break; },
-                _ => { result.push(ch as u8); },
+                b' ' | b'\t' | b'\n' => { break; },
+                _ => { result.push(ch); },
             }
             if let Some(ch2) = self.next_char() { ch = ch2; } else { break; }
         }
-        unsafe { Some(String::from_utf8_unchecked(result)) }
+        Some(OsString::from_raw_vec(result).expect("should never fail"))
     }
 
     fn parse_double(&mut self, result: &mut Vec<u8>) -> Result<(), ()> {
         loop {
             if let Some(ch2) = self.next_char() {
-                match ch2 as char {
-                    '\\' => {
+                match ch2 {
+                    b'\\' => {
                         if let Some(ch3) = self.next_char() {
-                            match ch3 as char {
+                            match ch3 {
                                 // \$ => $
-                                '$' | '`' | '"' | '\\' => { result.push(ch3); },
+                                b'$' | b'`' | b'"' | b'\\' => { result.push(ch3); },
                                 // \<newline> => nothing
-                                '\n' => {},
+                                b'\n' => {},
                                 // \x => =x
-                                _ => { result.push('\\' as u8); result.push(ch3); }
+                                _ => { result.push(b'\\'); result.push(ch3); }
                             }
                         } else {
                             return Err(());
                         }
                     },
-                    '"' => { return Ok(()); },
+                    b'"' => { return Ok(()); },
                     _ => { result.push(ch2); },
                 }
             } else {
@@ -107,8 +138,8 @@ impl<'a> Shlex<'a> {
     fn parse_single(&mut self, result: &mut Vec<u8>) -> Result<(), ()> {
         loop {
             if let Some(ch2) = self.next_char() {
-                match ch2 as char {
-                    '\'' => { return Ok(()); },
+                match ch2 {
+                    b'\'' => { return Ok(()); },
                     _ => { result.push(ch2); },
                 }
             } else {
@@ -119,22 +150,22 @@ impl<'a> Shlex<'a> {
 
     fn next_char(&mut self) -> Option<u8> {
         let res = self.in_iter.next();
-        if res == Some('\n' as u8) { self.line_no += 1; }
+        if res == Some(b'\n') { self.line_no += 1; }
         res
     }
 }
 
-impl<'a> Iterator for Shlex<'a> {
-    type Item = String;
-    fn next(&mut self) -> Option<String> {
+impl Iterator for ShlexOs {
+    type Item = OsString;
+    fn next(&mut self) -> Option<Self::Item> {
         if let Some(mut ch) = self.next_char() {
             // skip initial whitespace
             loop {
-                match ch as char {
-                    ' ' | '\t' | '\n' => {},
-                    '#' => {
+                match ch {
+                    b' ' | b'\t' | b'\n' => {},
+                    b'#' => {
                         while let Some(ch2) = self.next_char() {
-                            if ch2 as char == '\n' { break; }
+                            if ch2 == b'\n' { break; }
                         }
                     },
                     _ => { break; }
@@ -151,32 +182,32 @@ impl<'a> Iterator for Shlex<'a> {
 
 /// Convenience function that consumes the whole string at once.  Returns None if the input was
 /// erroneous.
-pub fn split(in_str: &str) -> Option<Vec<String>> {
-    let mut shl = Shlex::new(in_str);
+pub fn split(in_str: &OsStr) -> Option<Vec<OsString>> {
+    let mut shl = ShlexOs::new(in_str);
     let res = shl.by_ref().collect();
     if shl.had_error { None } else { Some(res) }
 }
 
 /// Given a single word, return a string suitable to encode it as a shell argument.
-pub fn quote(in_str: &str) -> Cow<str> {
+pub fn quote(in_str: &OsStr) -> Cow<OsStr> {
     if in_str.len() == 0 {
-        "\"\"".into()
-    } else if in_str.bytes().any(|c| match c as char {
-        '|' | '&' | ';' | '<' | '>' | '(' | ')' | '$' | '`' | '\\' | '"' | '\'' | ' ' | '\t' |
-        '\r' | '\n' | '*' | '?' | '[' | '#' | '~' | '=' | '%' => true,
+        OsStr::new("\"\"").into()
+    } else if in_str.to_raw_bytes().iter().any(|c| match *c {
+        b'|' | b'&' | b';' | b'<' | b'>' | b'(' | b')' | b'$' | b'`' | b'\\' | b'"' | b'\'' | b' ' | b'\t' |
+        b'\r' | b'\n' | b'*' | b'?' | b'[' | b'#' | b'~' | b'=' | b'%' => true,
         _ => false
     }) {
         let mut out: Vec<u8> = Vec::new();
         out.push('"' as u8);
-        for c in in_str.bytes() {
-            match c as char {
-                '$' | '`' | '"' | '\\' => out.push('\\' as u8),
+        for c in &*in_str.to_raw_bytes() {
+            match *c {
+                b'$' | b'`' | b'"' | b'\\' => out.push(b'\\'),
                 _ => ()
             }
-            out.push(c);
+            out.push(*c);
         }
-        out.push('"' as u8);
-        unsafe { String::from_utf8_unchecked(out) }.into()
+        out.push(b'"');
+        OsString::from_raw_vec(out).expect("should never fail").into()
     } else {
         in_str.into()
     }
@@ -184,11 +215,11 @@ pub fn quote(in_str: &str) -> Cow<str> {
 
 /// Convenience function that consumes an iterable of words and turns it into a single string,
 /// quoting words when necessary. Consecutive words will be separated by a single space.
-pub fn join<'a, I: IntoIterator<Item = &'a str>>(words: I) -> String {
-    words.into_iter()
+pub fn join<'a, I: IntoIterator<Item = &'a OsStr>>(words: I) -> OsString {
+    let vec = words.into_iter()
         .map(quote)
-        .collect::<Vec<_>>()
-        .join(" ")
+        .collect::<Vec<_>>();
+    MultiOsStrJoinerWrapper{ inner: vec }.into_joined(OsStr::new(" "))
 }
 
 #[cfg(test)]
@@ -218,15 +249,15 @@ static SPLIT_TEST_ITEMS: &'static [(&'static str, Option<&'static [&'static str]
 #[test]
 fn test_split() {
     for &(input, output) in SPLIT_TEST_ITEMS {
-        assert_eq!(split(input), output.map(|o| o.iter().map(|&x| x.to_owned()).collect()));
+        assert_eq!(split(input.as_ref()), output.map(|o| o.iter().map(|s| OsStr::new(s).to_os_string()).collect()));
     }
 }
 
 #[test]
 fn test_lineno() {
-    let mut sh = Shlex::new("\nfoo\nbar");
+    let mut sh = ShlexOs::new(OsStr::new("\nfoo\nbar"));
     while let Some(word) = sh.next() {
-        if word == "bar" {
+        if word == OsStr::new("bar") {
             assert_eq!(sh.line_no, 3);
         }
     }
@@ -234,16 +265,16 @@ fn test_lineno() {
 
 #[test]
 fn test_quote() {
-    assert_eq!(quote("foobar"), "foobar");
-    assert_eq!(quote("foo bar"), "\"foo bar\"");
-    assert_eq!(quote("\""), "\"\\\"\"");
-    assert_eq!(quote(""), "\"\"");
+    assert_eq!(quote(OsStr::new("foobar")), OsStr::new("foobar"));
+    assert_eq!(quote(OsStr::new("foo bar")), OsStr::new("\"foo bar\""));
+    assert_eq!(quote(OsStr::new("\"")), OsStr::new("\"\\\"\""));
+    assert_eq!(quote(OsStr::new("")), OsStr::new("\"\""));
 }
 
 #[test]
 fn test_join() {
-    assert_eq!(join(vec![]), "");
-    assert_eq!(join(vec![""]), "\"\"");
-    assert_eq!(join(vec!["a", "b"]), "a b");
-    assert_eq!(join(vec!["foo bar", "baz"]), "\"foo bar\" baz");
+    assert_eq!(join(vec![]), OsStr::new(""));
+    assert_eq!(join(vec![OsStr::new("")]), OsStr::new("\"\""));
+    assert_eq!(join(vec![OsStr::new("a"), OsStr::new("b")]), OsStr::new("a b"));
+    assert_eq!(join(vec![OsStr::new("foo bar"), OsStr::new("baz")]), OsStr::new("\"foo bar\" baz"));
 }
